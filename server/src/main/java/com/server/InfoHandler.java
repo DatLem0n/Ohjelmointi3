0e4b33836257d4331ef958b5906d12e1d0b18542
Ville Kujala <sahki232@gmail.com>
2024-02-11 21:41:01 +0200

    clean up test

---
+++ server/src/main/java/com/server/InfoHandler.java
@@ -0,0 +1,86 @@
+package com.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class InfoHandler implements HttpHandler {
+    private ArrayList<UserMessage> messages;
+    InfoHandler() {
+        messages = new ArrayList<UserMessage>();
+    }
+
+    /**
+     * Handle the given request and generate an appropriate response.
+     * See {@link HttpExchange} for a description of the steps
+     * involved in handling an exchange.
+     *
+     * @param exchange the exchange containing the request from the
+     *                 client and used to send the response
+     * @throws NullPointerException if exchange is {@code null}
+     * @throws IOException          if an I/O error occurs
+     */
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+            handlePOST(exchange);
+        } else if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
+            handleGET(exchange);
+        } else {
+            sendErrorMsg(exchange, HttpURLConnection.HTTP_NOT_IMPLEMENTED, "Not Supported");
+        }
+
+    }
+
+    private void handlePOST(HttpExchange exchange) throws  IOException{
+        InputStream body = exchange.getRequestBody();
+        String bodyText = new BufferedReader(new InputStreamReader(body, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+        JSONObject json = new JSONObject(bodyText);
+        body.close();
+        if (json.length() == 3){
+            try {
+                messages.add(new UserMessage(json.getString("locationName"), json.getString("locationDescription"), json.getString("locationCity")));
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, -1);
+            }catch (JSONException e){
+                sendErrorMsg(exchange, HttpURLConnection.HTTP_BAD_REQUEST, "Incorrect JSON data");
+            }
+        }
+        else {
+            sendErrorMsg(exchange, HttpURLConnection.HTTP_BAD_REQUEST, "Incorrect JSON length");
+        }
+
+    }
+
+    private void sendErrorMsg(HttpExchange exchange, int errorType, String message) throws IOException{
+        byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(errorType, bytes.length);
+        try (OutputStream output = exchange.getResponseBody()) {
+            output.write(bytes);
+        }
+    }
+
+    private void handleGET(HttpExchange exchange) throws  IOException{
+        JSONArray jsonArray = new JSONArray();
+
+        if (messages.isEmpty()){
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NO_CONTENT, -1);
+        }
+        for (UserMessage message : messages) {
+            jsonArray.put(message.toJSONObject());
+        }
+        byte[] bytes = jsonArray.toString().getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, bytes.length);
+        try (OutputStream output = exchange.getResponseBody()) {
+            output.write(bytes);
+        }
+    }
+}
--- server/src/main/java/com/server/Server.java
+++ server/src/main/java/com/server/Server.java
@@ -1,77 +1,17 @@
 package com.server;
 
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 import com.sun.net.httpserver.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.TrustManagerFactory;
 
-public class Server implements HttpHandler {
-
-    private ArrayList<UserMessage> messages;
-    private Server() {
-        messages = new ArrayList<UserMessage>();
-    }
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-
-        if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
-            handlePOST(exchange);
-        } else if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
-            handleGET(exchange);
-        } else {
-            sendErrorMsg(exchange, HttpURLConnection.HTTP_NOT_IMPLEMENTED, "Not Supported");
-        }
-
-    }
-
-    private void handlePOST(HttpExchange exchange) throws  IOException{
-        InputStream body = exchange.getRequestBody();
-        String bodyText = new BufferedReader(new InputStreamReader(body, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
-        JSONObject json = new JSONObject(bodyText);
-        body.close();
-        if (json.length() == 3){
-            try {
-                messages.add(new UserMessage(json.getString("locationName"), json.getString("locationDescription"), json.getString("locationCity")));
-                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, -1);
-            }catch (JSONException e){
-                sendErrorMsg(exchange, HttpURLConnection.HTTP_BAD_REQUEST, "Incorrect JSON data");
-            }
-        }
-        else {
-            sendErrorMsg(exchange, HttpURLConnection.HTTP_BAD_REQUEST, "Incorrect JSON length");
-        }
-
-    }
-
-    private void handleGET(HttpExchange exchange) throws  IOException{
-        JSONArray jsonArray = new JSONArray();
-
-        if (messages.isEmpty()){
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NO_CONTENT, -1);
-        }
-        for (UserMessage message : messages) {
-            jsonArray.put(message.toJSONObject());
-        }
-        byte[] bytes = jsonArray.toString().getBytes(StandardCharsets.UTF_8);
-        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, bytes.length);
-        try (OutputStream output = exchange.getResponseBody()) {
-            output.write(bytes);
-        }
-    }
+public class Server {
 
     private static SSLContext myServerSSLContext(String[] args) throws Exception{
         KeyStore ks = KeyStore.getInstance("JKS");
@@ -97,17 +37,6 @@ public class Server implements HttpHandler {
         return ssl;
     }
 
-
-
-    private void sendErrorMsg(HttpExchange exchange, int errorType, String message) throws IOException{
-        byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
-        exchange.sendResponseHeaders(errorType, bytes.length);
-        try (OutputStream output = exchange.getResponseBody()) {
-            output.write(bytes);
-        }
-    }
-
-
     public static void main(String[] args) throws Exception {
         try{
         HttpsServer server = HttpsServer.create(new InetSocketAddress(8001),0);
@@ -122,7 +51,7 @@ public class Server implements HttpHandler {
         });
 
         UserAuthenticator authenticator = new UserAuthenticator("/info");
-        HttpContext infoContext = server.createContext("/info", new Server());
+        HttpContext infoContext = server.createContext("/info", new InfoHandler());
         HttpContext registrationContext = server.createContext("/registration", new RegistrationHandler(authenticator));
         infoContext.setAuthenticator(authenticator);
         server.setExecutor(null);
